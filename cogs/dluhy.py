0ec91ba3141a566fb3cc79bd3909a686de4bcfcf
Marián Marek <30873527+flowernal@users.noreply.github.com>
2026-03-04 20:32:42 +0100

    fix: typo

--- cogs/dluhy.py
+++ cogs/dluhy.py
@@ -54,7 +54,7 @@ class Dluhy(commands.Cog):
             zprava += f"🤑 {format_debt(d)}\n"
             
         zprava += "\n"
-        zprava += f"Celkový dluh dluh:\n"
+        zprava += f"Celkový dluh:\n"
         total = sum_by_currency(active)
         for cur, total_amount in total.items():
             zprava += f"🔥✍ {cur}: {total_amount:.2f}\n"
